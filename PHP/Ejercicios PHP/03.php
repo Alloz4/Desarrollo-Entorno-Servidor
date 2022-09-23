331007c1332e6cbe6891998bc4b81c2e66631327
Marcos <marcos.alloza4@gmail.com>
2022-09-23 12:22:04 +0200

    Actualizado 03

--- PHP/Ejercicios PHP/03.php
+++ PHP/Ejercicios PHP/03.php
@@ -1,31 +1,38 @@
+<!-- Obtener un número al azar entre 1 y 9 y generar una pirámide con ese número de peldaños.
+Utilizar la marca <code></code> para que la visualización no se deforme por el tamaño de los espacio o una estilo con tipo de letra monospace.
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php 
+    <?php
 
-$numero = random_int(1,9);
-$espacios = $numero - 1;
-echo "<code>";
-//
-for ($i=1; $i <= $numero; $i++) { 
-    for ($j=$espacios; $j > 0 ; $j--) { 
-        echo "&nbsp;";
+    $numero = random_int(1, 9);
+    $espacios = $numero - 1;
+    echo "<code>";
+
+    for ($i = 1; $i <= $numero; $i++) {
+        for ($j = $espacios; $j > 0; $j--) {
+            echo "&nbsp;";
+        }
+        $espacios--;
+        for ($k = 0; $k <= 2 * $i - 1; $k++) {
+            echo "*";
+        }
+        echo "<br>";
     }
-    $espacios--;
-    for ($k=0; $k <=2*$i-1; $k++) { 
-        echo "*";
-    }
-    echo "<br>";
-}
 
-echo "</code>";
+    echo "</code>";
 
-?>
+    ?>
 </body>
+
 </html>
