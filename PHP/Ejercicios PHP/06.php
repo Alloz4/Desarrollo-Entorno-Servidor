cc85518bf2cb2f1de7f117a5fa62145d1761a4d4
Marcos <marcos.alloza4@gmail.com>
2022-09-23 12:06:45 +0200

    Create Ejercicio06

---
+++ PHP/Ejercicios PHP/06.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 style="background-color: blue; color: white; text-align: center;">TABLA DE MULTIPLICAR</h1>
+    <table>
+        <?php
+        $num = random_int(1, 10);
+        ?>
+        <tr>
+            <td>Tabla del <?php echo $num ?></td>
+            <td><?php echo " "; ?></td>
+        </tr>
+        <?php
+        for ($i = 0; $i <= 10; $i++) {
+            echo "<tr>";
+            echo "<td>$num x $i = </td>";
+            echo "<td>" . $num * $i . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <style>
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+        }
+
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+        }
+    </style>
+</body>
+
+</html>
