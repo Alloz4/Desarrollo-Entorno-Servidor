f983d3e4875ca4e2d3735ab213fc92b9f13c6b7b
Marcos <marcos.alloza4@gmail.com>
2022-09-23 12:19:03 +0200

    Actualizado Ejercicio 02

--- PHP/Ejercicios PHP/02.php
+++ PHP/Ejercicios PHP/02.php
@@ -1,26 +1,30 @@
+<!-- Obtener un número al azar entre 1 y 9 y generar una la escalera numérica del tamaño indicado alternando colores entre rojo y azul. -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    $numero = random_int(1,9);
-    for ($i=0; $i <= $numero; $i++) { 
-        for ($j=0; $j < $i ; $j++) { 
-            if ($i % 2 == 0){
-                echo "<span style='color: blue;'>".$i."</span>";
+    <?php
+    $numero = random_int(1, 9);
+    for ($i = 0; $i <= $numero; $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            if ($i % 2 == 0) {
+                echo "<span style='color: blue;'>" . $i . "</span>";
             } else {
-                echo "<span style='color: red'>".$i."</span>";
+                echo "<span style='color: red'>" . $i . "</span>";
             }
         }
         echo "<br>";
-    }   
-    
+    }
+
     ?>
 
 </body>
+
 </html>
