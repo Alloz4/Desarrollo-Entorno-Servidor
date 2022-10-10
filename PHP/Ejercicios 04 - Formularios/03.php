cfd2e79e6501c0d8a7ea3242fbcd49de01171d58
Marcos <marcos.alloza4@gmail.com>
2022-10-10 16:53:08 +0200

    Update Ejercicios 04

--- PHP/Ejercicios 04 - Formularios/03.php
+++ PHP/Ejercicios 04 - Formularios/03.php
@@ -1,1 +1,12 @@
 <?php
+
+$datos = $_REQUEST;
+print_r($datos);
+echo "<br><br>";
+var_dump($datos);
+echo "<br><br>";
+
+foreach ($datos as $key => $value) {
+    echo $key . " => " . $value;
+    echo "<br>";
+}
