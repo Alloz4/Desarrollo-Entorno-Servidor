0f0f4a1e92943091d0d7542b7d14fe91da710353
Marcos <marcos.alloza4@gmail.com>
2022-11-06 17:13:25 +0100

    Ejercicios 06

--- PHP/Ejercicios 6 - Sesiones & Cookies/02.php
+++ PHP/Ejercicios 6 - Sesiones & Cookies/02.php
@@ -1,22 +1,37 @@
 <?php
 session_start();
+define('INTENTOS_MAX', 5);
 
 if (!isset($_SESSION['numeroOculto'])) {
     $_SESSION['numeroOculto'] = random_int(1, 20);
+    $_SESSION['intentos'] = 0;
     echo "<h1>¡Bienvenido!</h1>";
 } else {
     if (isset($_REQUEST['numeroOculto'])) {
-        echo "El número es: " . $_SESSION['numeroOculto'] . "<br>";
         if ($_SESSION['numeroOculto'] == $_REQUEST['numeroOculto']) {
             echo "Enhorabuena, has acertado! <br>";
             session_destroy();
             header("Refresh: 3");
-        } else {
-            echo "Has fallado!";
+            exit();
+        } else if ($_SESSION['numeroOculto'] != $_REQUEST['numeroOculto']) {
+            echo "¡Has fallado! <br>";
+            $_SESSION['intentos']++;
+            echo "Te quedan " . (INTENTOS_MAX - $_SESSION['intentos']) . "<br>";
+            if ($_SESSION['numeroOculto'] > $_REQUEST['numeroOculto']) {
+                echo "El número que intentas adivinar es mayor. <br>";
+            } else {
+                echo "El número que intentas adivinar es menor. <br>";
+            }
+        }
+        if ($_SESSION['intentos'] === INTENTOS_MAX) {
+            echo "Has agotado todos los intentos. <br>";
+            session_destroy();
+            header("Refresh: 3");
+            exit();
         }
     }
 }
 ?>
-<form method="get">
+<form method="get" action="02.php">
     Introduce un número: <input type="text" name="numeroOculto">
 </form>
