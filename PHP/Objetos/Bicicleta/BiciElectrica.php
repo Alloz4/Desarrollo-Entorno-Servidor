6e132077897deda4b957da7f2a09671f89273ef5
Marcos <marcos.alloza4@gmail.com>
2022-11-28 03:30:03 +0100

    repaso examen

--- PHP/Objetos/Bicicleta/BiciElectrica.php
+++ PHP/Objetos/Bicicleta/BiciElectrica.php
@@ -8,25 +8,40 @@ class BiciElectrica
     public $bateria; // Carga de la batería en tanto por ciento (entero)
     public $operativa; // Estado de la bicleta ( true operativa- false no disponible)
 
-    function ___get($propiedad)
+
+    // Constructor
+    function __construct($id, $coordx, $coordy, $bateria, $operativa)
+    {
+        $this->id = $id;
+        $this->coordx = $coordx;
+        $this->coordy = $coordy;
+        $this->bateria = $bateria;
+        $this->operativa = $operativa;
+    }
+
+    //Función __get para acceder a los atributos de la clase
+    function __get($propiedad)
     {
         if (property_exists($this, $propiedad)) {
             return $this->$propiedad;
         }
     }
 
-    function ___set($propiedad, $valor)
+    //Función __set para modificar los atributos de la clase
+    function __set($propiedad, $valor)
     {
         if (property_exists($this, $propiedad)) {
             $this->$propiedad = $valor;
         }
     }
 
+    //Función para mostrar los datos de la bicicleta
     function __toString()
     {
         return "BiciElectrica: " . $this->id . " Batería: " . $this->bateria . "% ";
     }
 
+    //Función para sacar la distancia entre dos puntos
     function distancia($x, $y)
     {
         return sqrt(pow($this->coordx - $x, 2) + pow($this->coordy - $y, 2));
